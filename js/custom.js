6e0f53d720589715c603b3f827c890f72bbb16ca
RainbowWorks <kuj4235@kakao.com>
2025-05-06 21:26:11 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,94 @@
+$(function () {
+
+
+    const MVS = new Swiper('.main_visual_slide', {
+        loop: true,
+        parallax: true,
+        //effect: 'fade', /*이미지에fade주기(effect: 'fade',)*/
+        speed: 1000,
+        pagination: {
+            el: '.main_visual .page',
+            clickable: true, /*클릭안되면 쓰면된다.*/
+        },/*도트만들때필요함*/
+
+    });
+
+
+    const main_manual_slide = new Swiper('.main_manual_slide', {
+        loop: true,
+        slidesPerView: 3.5,
+        spaceBetween: 30,
+    });
+
+
+
+
+    $('.main_manual .step button').on('click', function () {
+        let idx = $(this).parent().index();
+        main_manual_slide.slideToLoop(idx);
+    })
+
+
+    /*autoplay.start*/
+
+    const MBS = new Swiper('.main_brand_slide', {
+        loop: true,
+        effect: 'fade', /*이미지에fade주기(effect: 'fade',)*/
+        pagination: {
+            el: '.main_brand .page',
+            clickable: true, /*클릭안되면 쓰면된다.*/
+        },/*도트만들때필요함*/
+        navigation: {
+            nextEl: '.main_brand .next',
+            prevEl: '.main_brand .prev',
+        },/*화살표*/
+    });
+
+
+});
+
+
+
+$(function () {
+    $(window).on('scroll', function () {
+        let sct = $(window).scrollTop();
+
+        if (sct > 0) {
+            $('#header').addClass('on');
+        } else {
+            $('#header').removeClass('on');
+        }
+    });
+});
+
+$(function () {
+    $('.main_models .tab_models button').on('click', function () {
+        let idx = $(this).index(); // 클릭한 것의 번호를 가져와서 저장함
+        console.log(idx);
+
+        $('.main_models .tab_models button').removeClass('on');
+        $(this).addClass('on');
+
+        $('.main_models .tab_models_content .con').removeClass('on');
+        $('.main_models .tab_models_content .con').eq(idx).addClass('on');
+    });
+});
+
+
+$(function () {
+    $('.to_top').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 })
+    });
+
+    $(window).on('scroll', function () {
+        //스크롤 된 양을 구함.
+        let sct = $(window).scrollTop();
+        console.log(sct)
+
+        if (sct > 200) {
+            $('.to_top').addClass('on')
+        } else {
+            $('.to_top').removeClass('on')
+        }
+    });
+});
